9676b8cebcae92f985074a77922458643da99e76
TomKimber <thomas.kimber@hmcts.net>
2021-10-01 13:41:04 +0100

    Fixed dynatrace_settings typo.

--- locals.tf
+++ locals.tf
@@ -1,3 +1,7 @@
 locals {
-  dynatrace_settings = var.hostgroup == null && var.server == null ? "{ \"tenantId\" : \"${var.tenant_id}\" , \"token\" : \"${var.token}\" }" : var.hostgroup != null && var.server == null ? "{ \"tenantId\" : \"${var.tenant_id}\" , \"token\" : \"${var.token}\" , \"hostgroup\" : \"${var.hostgroup}\" }" : var.hostgroup == null && var.server != null ? "{ \"tenantId\" : \"${var.tenant_id}\" , \"token\" : \"${var.token}\" , \"server\" : \"${var.server}\" }" : "{ \"tenantId\" : \"${var.tenant_id}\" , \"token\" : \"${var.token}\" , \"hostGroup\" : \"${var.hostgroup}\" , \"server\" : \"${var.server}\" }"
+  dynatrace_settings = var.hostgroup == null && var.server == null ? "{ \"tenantId\" : \"${var.tenant_id}\" , \"token\" : \"${var.token}\" }" : var.hostgroup != null && var.server == null ? "{ \"tenantId\" : \"${var.tenant_id}\" , \"token\" : \"${var.token}\" , \"hostGroup\" : \"${var.hostgroup}\" }" : var.hostgroup == null && var.server != null ? "{ \"tenantId\" : \"${var.tenant_id}\" , \"token\" : \"${var.token}\" , \"server\" : \"${var.server}\" }" : "{ \"tenantId\" : \"${var.tenant_id}\" , \"token\" : \"${var.token}\" , \"hostGroup\" : \"${var.hostgroup}\" , \"server\" : \"${var.server}\" }"
+}
+
+output "dynatrace_settings" {
+  value = local.dynatrace_settings
 }
